cb95195897f59d65c8e78546327e75686a9afc69
Wahab Sheikh <wahabmsheikh@gmail.com>
2015-01-14 09:04:20 -0800

    Add router

--- lib/phase6/router.rb
+++ lib/phase6/router.rb
@@ -1,28 +1,44 @@
+require "active_support"
+
 module Phase6
   class Route
     attr_reader :pattern, :http_method, :controller_class, :action_name
 
     def initialize(pattern, http_method, controller_class, action_name)
+      @pattern = pattern
+      @http_method = http_method
+      @controller_class = controller_class
+      @action_name = action_name
     end
 
     # checks if pattern matches path and method matches request method
     def matches?(req)
+      !!(req.path.match(pattern)) && (http_method == req.request_method.downcase.to_sym)
     end
 
     # use pattern to pull out route params (save for later?)
     # instantiate controller and call controller action
     def run(req, res)
+      route_params = {}
+      keys = req.path.match(pattern).names
+      vals = req.path.match(pattern).captures
+      (0...keys.length).each { |i| route_params[keys[i]] = vals[i] }
+      controller_instance = controller_class.new(req, res, route_params)
+      controller_instance.invoke_action(action_name)
     end
+
   end
 
   class Router
     attr_reader :routes
 
     def initialize
+      @routes = []
     end
 
     # simply adds a new route to the list of routes
     def add_route(pattern, method, controller_class, action_name)
+      @routes << Route.new(pattern, method, controller_class, action_name)
     end
 
     # evaluate the proc in the context of the instance
