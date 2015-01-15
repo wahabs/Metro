337954cc40c7f19030c44df94e1091fcc12efdd1
Wahab Sheikh <wahabmsheikh@gmail.com>
2015-01-14 17:06:05 -0800

    Fix flash spec

--- spec/flash_spec.rb
+++ spec/flash_spec.rb
@@ -1,74 +1,146 @@
 require 'webrick'
+require 'flash'
 require 'controller_base'
-require 'router'
-require 'byebug'
 
-describe "the symphony of things" do
+describe Flash do
   let(:req) { WEBrick::HTTPRequest.new(Logger: nil) }
   let(:res) { WEBrick::HTTPResponse.new(HTTPVersion: '1.0') }
+  let(:cook) do
+    WEBrick::Cookie.new('_rails_lite_app_flash', { xyz: 'abc' }.to_json)
+  end
 
+  it "deserializes json cookie if one exists" do
+    req.cookies << cook
+    flash = Flash.new(req)
+    expect(flash['xyz']).to eq('abc')
+  end
+
+  describe '#now' do
+    it 'allows access to values with :[]' do
+      flash = Flash.new(req)
+      expect(flash.now).to respond_to(:[])
+    end
+
+    it 'does not persist beyond first request' do
+      flash = Flash.new(req)
+      flash.now[:errors] = "Error"
+      flash.store_flash(res)
+      simulate_req_res!(req, res)
+      flash2 = Flash.new(req)
+      expect(flash2[:errors]).to be_nil
+      expect(flash2.now[:errors]).to be_nil
+    end
+
+    it 'allows access to data via the top level flash :[]' do
+      flash = Flash.new(req)
+      flash.now[:errors] = "Error"
+      expect(flash[:errors]).to eq("Error")
+    end
+  end
+
+  describe "#store_flash" do
+    context "without cookies in request" do
+      before(:each) do
+        flash = Flash.new(req)
+        flash['first_key'] = 'first_val'
+        flash.store_flash(res)
+      end
+
+      it "adds new cookie with '_rails_lite_app_flash' name to response" do
+        cookie = res.cookies.find { |c| c.name == '_rails_lite_app_flash' }
+        expect(cookie).not_to be_nil
+      end
+
+      it "stores the cookie in json format" do
+        cookie = res.cookies.find { |c| c.name == '_rails_lite_app_flash' }
+        expect(JSON.parse(cookie.value)).to be_instance_of(Hash)
+      end
+    end
+
+    context "with cookies in request" do
+      before(:each) do
+        cook = WEBrick::Cookie.new('_rails_lite_app_flash', { pho: "soup" }.to_json)
+        req.cookies << cook
+      end
+
+      it "reads the pre-existing cookie data into hash" do
+        flash = Flash.new(req)
+        expect(flash['pho']).to eq('soup')
+      end
+    end
+  end
+
+  it "can be accessed using either strings or symbols" do
+    flash = Flash.new(req)
+    flash["notice"] = "test"
+    expect(flash[:notice]).to eq("test")
+  end
+
+  it "only saves data stored directly in flash for one more request" do
+    flash = Flash.new(req)
+    flash['first_request'] = 'first'
+    flash.store_flash(res)
+
+    simulate_req_res!(req, res)
+
+    flash2 = Flash.new(req)
+    flash2.store_flash(res)
+    expect(flash2['first_request']).to eq('first')
+
+    simulate_req_res!(req, res)
+
+    flash3 = Flash.new(req)
+    expect(flash3['first_request']).to be_nil
+  end
+end
+
+describe ControllerBase do
   before(:all) do
-    class Ctrlr < ControllerBase
-      def route_render
-        render_content("testing", "text/html")
-      end
-
-      def route_does_params
-        render_content("got ##{ params["id"] }", "text/text")
-      end
-
-      def update_session
-        session[:token] = "testing"
-        flash[:message] = "Here's a flash"
-        render_content("hi", "text/html")
-      end
-
+    class CatsController < ControllerBase
     end
   end
-  after(:all) { Object.send(:remove_const, "Ctrlr") }
+  after(:all) { Object.send(:remove_const, "CatsController") }
 
-  describe "routes and params" do
-    it "route instantiates controller and calls invoke action" do
-      route = Route.new(Regexp.new("^/statuses/(?<id>\\d+)$"), :get, Ctrlr, :route_render)
-      req.stub(:path) { "/statuses/1" }
-      req.stub(:request_method) { :get }
-      route.run(req, res)
-      res.body.should == "testing"
+  let(:req) { WEBrick::HTTPRequest.new(Logger: nil) }
+  let(:res) { WEBrick::HTTPResponse.new(HTTPVersion: '1.0') }
+  let(:cats_controller) { CatsController.new(req, res) }
+
+  describe "#flash" do
+    it "returns a flash instance" do
+      expect(cats_controller.flash).to be_a(Flash)
     end
 
-    it "route adds to params" do
-      route = Route.new(Regexp.new("^/statuses/(?<id>\\d+)$"), :get, Ctrlr, :route_does_params)
-      req.stub(:path) { "/statuses/1" }
-      req.stub(:request_method) { :get }
-      route.run(req, res)
-      res.body.should == "got #1"
+    it "returns the same instance on successive invocations" do
+      first_result = cats_controller.flash
+      expect(cats_controller.flash).to be(first_result)
     end
   end
 
-  describe "controller sessions" do
-    let(:ctrlr) { Ctrlr.new(req, res) }
-
-    it "exposes a session via the session method" do
-      ctrlr.session.should be_instance_of(Session)
+  shared_examples_for "storing flash data" do
+    it "should store the flash data" do
+      cats_controller.flash['test_key'] = 'test_value'
+      cats_controller.send(method, *args)
+      cookie = res.cookies.find { |c| c.name == '_rails_lite_app_flash' }
+      h = JSON.parse(cookie.value)
+      expect(h['test_key']).to eq('test_value')
     end
-
-    it "saves the session and flash after rendering content" do
-      ctrlr.update_session
-      res.cookies.count.should == 2 # flash and session cookies
-      session = JSON.parse((res.cookies.find { |cookie| cookie.name == '_rails_lite_app' }).value)
-      session["token"].should == "testing"
-      ctrlr.flash["message"].should == "Here's a flash"
-    end
-
-    it "should preserve flash but not flash.now on the next request" do
-      ctrlr.flash["message"] = "Flash!"
-      ctrlr.flash.now["temp_message"] = "Flash now!"
-      ctrlr.flash["temp_message"].should == "Flash now!"
-      ctrlr.route_render
-      flash = JSON.parse((res.cookies.find { |cookie| cookie.name == '_rails_lite_app_flash' }).value)
-      flash["message"].should == "Flash!"
-      flash["temp_message"].should be_nil
-    end
-
   end
+
+  describe "#render_content" do
+    let(:method) { :render_content }
+    let(:args) { ['test', 'text/plain'] }
+    include_examples "storing flash data"
+  end
+
+  describe "#redirect_to" do
+    let(:method) { :redirect_to }
+    let(:args) { ['http://appacademy.io'] }
+    include_examples "storing flash data"
+  end
+end
+
+def simulate_req_res!(req, res)
+  req.cookies.delete_if { true }
+  req.cookies.concat(res.cookies)
+  res.cookies.delete_if { true }
 end
